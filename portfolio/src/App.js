ab1b8b70156db2185ed6a0923580c56eba2df4e7
Michael Hancock <shanehancock64@live.com>
2022-07-29 20:07:07 -0700

    setup
    nav bar component

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar";
+
+
+function App() {
+  return (
+    <div>
+    <NavBar></NavBar>
+    </div> 
+    
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#83A1CD]">
+
+    </div>
+  )
+}
+
+export default NavBar
