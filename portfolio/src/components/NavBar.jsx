6a0db67d25787ab957a6e7632caaa28a08ff46f6
Michael Hancock <shanehancock64@live.com>
2022-08-16 16:46:36 -0700

    done

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -4,8 +4,8 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook } from 'react-icons/f
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 import {Link} from 'react-scroll'
-import { Document, Page } from 'react-pdf';
-
+import { Document} from 'react-pdf';
+import {resume} from '../assets/resume.pdf'
 function NavBar() {
   const [nav, setNav] = useState(false)
   const handleClick = () => setNav(!nav)
@@ -68,8 +68,9 @@ function NavBar() {
             </li>
           
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-              <a className='flex justify-between items-center w-full text-gray-300' href="/">
+              <a className='flex justify-between items-center w-full text-gray-300' href="">
                 Resume <BsFillPersonLinesFill  size={30}/>
+                
                 
               </a>
             </li>
